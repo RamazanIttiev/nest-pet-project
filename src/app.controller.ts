1a542e1bbbd5750cceae91bb108feb783c1ae222
Ramazan Ittiev <rittiev@devexperts.com>
2022-06-18 14:32:01 +0300

    [4][Server] Implemented Login route

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+
+@Controller()
+export class AppController {
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { AuthService } from './auth/auth.service';
+
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AuthService } from './auth/auth.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
   providers: [AuthService],
+  controllers: [AppController],
+  imports: [UsersModule, AuthModule],
 })
 export class AppModule {}
