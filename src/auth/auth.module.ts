463fb83069229f389058974b7d61a402e1c76bd9
Ramazan Ittiev <rittiev@devexperts.com>
2022-06-18 13:29:03 +0300

    [4][Server] Implemented Passport local

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+
+import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+
 import { User, UsersService } from '../users/users.service';
 
 @Injectable()
@@ -12,7 +13,7 @@ export class AuthService {
   async validateUser(
     username: string,
     pass: string,
-  ): Promise<Omit<User, 'password'>> {
+  ): Promise<Omit<User, 'password'> | null> {
     const user = await this.usersService.findUser(username);
     if (user && user.password === pass) {
       const { password, ...validatedUser } = user;
