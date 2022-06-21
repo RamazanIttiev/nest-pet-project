4b48566212bc077e2c3cf4ea5d188b12211451b3
Ramazan Ittiev <rittiev@devexperts.com>
2022-06-21 16:23:55 +0300

    [11][Client] added global styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/app';
 
+import './index.css';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
