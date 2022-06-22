17e994f7d316716858704a2e44f492703881b44c
Ramazan Ittiev <rittiev@devexperts.com>
2022-06-22 11:20:53 +0300

    [13][Client] sign-up-aside page added

--- client/src/views/sign-up/components/sign-up-aside.component.tsx
+++ client/src/views/sign-up/components/sign-up-aside.component.tsx
@@ -3,18 +3,7 @@ import React, { FC } from 'react';
 export const SignUpAsideComponent: FC = () => {
 	return (
 		<div className="aside">
-			<p className="aside-text">
-				It is one of the oldest modern frameworks, with more than 10 years in the npm repository. Although it
-				has changed a lot, it still has compatibility with most of the previous versions. And all the changes
-				have been to the better. Some say that React with hooks has created an even better framework.
-			</p>
-			<br />
-			<p className="aside-text">
-				But the best quality of React is not that it has hooks, or any visible functionality, rather the
-				opposite. React has pushed to the latest standards of JavaScript, and it has pushed for JSX. React is no
-				longer a framework, maybe it never was, it is just a library. It has pushed so hard for standards, that
-				it eventually has removed itself from the user code.
-			</p>
+			<p className="aside-text">Welcome :)</p>
 		</div>
 	);
 };
