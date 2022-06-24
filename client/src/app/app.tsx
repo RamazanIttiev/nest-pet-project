96ff881a0711bed2971b9c521b712da7b0d8aceb
Ramazan Ittiev <rittiev@devexperts.com>
2022-06-24 16:13:20 +0300

    [25][Client] added react router and home page

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,11 +1,16 @@
 import React, { FC } from 'react';
 
 import { SignUpPage } from '../pages/sign-up.page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from '../pages/home.page';
 
 export const App: FC = () => {
 	return (
-		<>
-			<SignUpPage />
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/register" element={<SignUpPage />} />
+			</Routes>
+		</BrowserRouter>
 	);
 };
