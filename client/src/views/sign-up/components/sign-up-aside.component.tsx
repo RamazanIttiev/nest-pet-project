1590f6948ee10bfe087a86c335e346e76bc09ee8
Ramazan Ittiev <rittiev@devexperts.com>
2022-06-27 13:47:15 +0300

    [29][Fixes] fixes

--- client/src/views/sign-up/components/sign-up-aside.component.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-export const SignUpAsideComponent: FC = () => {
-	return (
-		<div className="aside">
-			<p className="aside-text">Welcome :)</p>
-		</div>
-	);
-};
