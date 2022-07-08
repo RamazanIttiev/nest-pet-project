162381cab89db3557c71cfcb57d75a0d248bb3dd
Ramazan Ittiev <rittiev@devexperts.com>
2022-07-08 17:31:07 +0300

    [41][Fixes] fixes

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,11 @@ import { cert, initializeApp } from 'firebase-admin/app';
 import * as serviceAccount from '../firebase-adminsdk.json';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { cors: true });
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+	});
 	await app.listen(3001);
 }
 
