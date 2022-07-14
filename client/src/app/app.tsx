ac35bfcc80a6816a5d9f3592c73b728b05fc18e4
Ramazan Ittiev <rittiev@devexperts.com>
2022-07-14 10:46:46 +0300

    [53][Fixes] fetch data fixes

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Header } from '../views/header/header';
 import { HomeContainer } from '../views/home/containers/home.container';
 import { SignUpContainer } from '../views/sign-up/containers/sign-up.container';
@@ -8,26 +8,29 @@ import { ProfileContainer } from '../views/profile/containers/profile.container'
 import { ProfileData } from '../models/profile.model';
 
 export const App: FC = () => {
+	const location = useLocation();
 	const [profileData, setProfileData] = useState<ProfileData>({ phone: '', username: '', reminders: [] });
 
 	useEffect(() => {
-		fetch('http://localhost:3001/profile', {
-			method: 'GET',
-			credentials: 'include',
-		})
-			.then(response => {
-				return response.json();
+		if (location.pathname === '/profile') {
+			fetch('http://localhost:3001/profile', {
+				method: 'GET',
+				credentials: 'include',
 			})
-			.then(profileData => {
-				return setProfileData(profileData);
-			})
-			.catch(error => {
-				console.log(error);
-			});
-	}, []);
-	console.log(profileData.reminders);
+				.then(response => {
+					return response.json();
+				})
+				.then(profileData => {
+					return setProfileData(profileData);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
+	}, [location.pathname]);
+
 	return (
-		<BrowserRouter>
+		<>
 			<Header />
 			<Routes>
 				<Route index element={<HomeContainer />} />
@@ -35,6 +38,6 @@ export const App: FC = () => {
 				<Route path="/login" element={<LoginContainer />} />
 				<Route path="/profile" element={<ProfileContainer reminders={profileData.reminders} />} />
 			</Routes>
-		</BrowserRouter>
+		</>
 	);
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/app';
 import { CssBaseline } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<CssBaseline />
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>,
 );
