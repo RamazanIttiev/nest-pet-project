8263ae8b26ff982b1ffb320ad2e9053bdcdbc58d
Ramazan Ittiev <rittiev@devexperts.com>
2023-01-27 10:52:02 +0300

    added static files to server folder and added Procfile for heroku usage

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
+import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { TasksService } from './tasks/tasks.service';
 import { TasksController } from './tasks/tasks.controller';
-import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-	imports: [AuthModule, UsersModule, TasksModule, ConfigModule.forRoot({ isGlobal: true })],
+	imports: [AuthModule, UsersModule, TasksModule, ConfigModule.forRoot({ isGlobal: true }),  ServeStaticModule.forRoot({
+		rootPath: join(__dirname, '../../client'),
+	}),],
 	providers: [TasksService],
 	controllers: [TasksController],
 })
